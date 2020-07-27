4d08fb8a5c27139d20f61a827d24771b98b4823f
Ankit Das <63000899+Ankit-SIT@users.noreply.github.com>
2020-07-27 15:33:28 +0530

    added part1.c
    Added Part 1

--- Assignment1/part1.c
+++ Assignment1/part1.c
@@ -44,69 +44,122 @@ void triangle(int n){
      printf("%d ",i*(i+1)/2);  
 }
 
-void inWord(){  
- int n = 0;
- scanf("%d",&n);                                
- int digits = (int)log10(n);                    // find the number of digits
- n  = reverse(n);                               // reverse the digits
- digits = digits - ((int)log10(n));             // this eliminates trailing zeros
-
- while(n){
-    switch(n % 10)
-    {      
-    case 0:
-        printf("ZERO ");
-        break;
-        
-    case 1:
-        printf("ONE ");
-        break;
-    
-    case 2:
-        printf("TWO ");
-        break;
-        
-    case 3:
-        printf("THREE ");
-        break;
-        
-    case 4:
-        printf("FOUR ");
-        break;
-        
-    case 5:
-        printf("FIVE ");
-        break;
-        
-    case 6:
-        printf("SIX ");
-        break;
-        
-    case 7:
-        printf("SEVEN ");
-        break;
-        
-    case 8:
-        printf("EIGHT ");
-        break;
-    
-    case 9:
-        printf("NINE ");
-        break;
  
-    default:
-        printf("ERROR! INVALID INPUT");
-        break; 
-    }
-    n /= 10;                       
-  }                                                // once done printing all digits
-  
-  while(digits){                                   // print all remaining zeros
-      printf("ZERO ");
-      digits--;
-  }
-}
+void inWord(int num){
 
+ int div, n;
+ int flag, digit, pos, len;
+
+ if(num == 0) {
+ printf("Zeron\n");
+ }
+ 
+ if(num > 99999) {
+ printf("please enter a number between 0 and 100000\n\n");
+ }
+ 
+ len = 0;
+ div = 1;
+ n = num;
+ 
+ while ( n > 9 ) {                     // find length of number
+ n /= 10;
+ div = div * 10;
+ len++;
+ }
+ 
+ len++;
+ pos = len;
+ 
+ while ( num != 0 ) {
+  digit = num / div;   // get individual digits
+  num = num % div;
+  div = div / 10;
+
+ switch(pos) {
+            case 2:                        // 2 digit number
+            case 5:                        // 5 digit number
+                if ( digit == 1 )
+                flag = 1;
+                else {
+                    flag = 0;
+                    switch(digit) {
+                        case 2: printf("twenty ");break;
+                        case 3: printf("thirty ");break;
+                        case 4: printf("forty ");break;
+                        case 5: printf("fifty ");break;
+                        case 6: printf("sixty ");break;
+                        case 7: printf("seventy ");break;
+                        case 8: printf("eighty ");break;
+                        case 9: printf("ninty ");
+                        }
+                    }
+            break;
+            case 1:                              // 1 or 4 digit number
+            case 4:
+                if (flag == 1) {
+                    flag = 0;
+                    switch(digit) {
+                    case 0 : printf("ten ");break;
+                    case 1 : printf("eleven ");break;
+                    case 2 : printf("twelve ");break;
+                    case 3 : printf("thirteen ");break;
+                    case 4 : printf("fourteen ");break;
+                    case 5 : printf("fifteen ");break;
+                    case 6 : printf("sixteen ");break;
+                    case 7 : printf("seventeen ");break;
+                    case 8 : printf("eighteen ");break;
+                    case 9 : printf("nineteen ");
+                    }
+                }   
+            
+                else {
+                    switch(digit) {
+                    case 1 : printf("one ");break;
+                    case 2 : printf("two ");break;
+                    case 3 : printf("three ");break;
+                    case 4 : printf("four ");break;
+                    case 5 : printf("five ");break;
+                    case 6 : printf("six ");break;
+                    case 7 : printf("seven ");break;
+                    case 8 : printf("eight ");break;
+                    case 9 : printf("nine ");
+                }
+            } 
+            
+            if (pos == 4) 
+                printf("thousand ");
+            break;
+            
+            case 3:                           // three digit number
+                if (digit > 0) {
+                    switch(digit) {
+                    case 1 : printf("one ");break;
+                    case 2 : printf("two ");break;
+                    case 3 : printf("three ");break;
+                    case 4 : printf("four ");break;
+                    case 5 : printf("five ");break;
+                    case 6 : printf("six ");break;
+                    case 7 : printf("seven ");break;
+                    case 8 : printf("eight ");break;
+                    case 9 : printf("nine ");
+                }
+            printf("hundred ");
+            }
+            break;
+        }
+        pos--;
+        }
+
+    if (pos == 4 && flag == 0)   
+        printf("thousand");
+
+    else if (pos == 4 && flag == 1)
+        printf("ten thousand");
+            
+    if (pos == 1 && flag == 1)
+        printf("ten ");
+ }  
 
 void main(){
      int choice = 0, run = 1;
@@ -114,7 +167,7 @@ void main(){
      
     while(run)
     {
-     printf("\n--MENU--\n");
+     printf("--MENU--\n");
      printf("1. SUM OF TWO NUMBERS\n");
      printf("2. REVERSE OF NUMBER\n");
      printf("3. AVERAGE OF NUMBERS\n");
@@ -149,7 +202,8 @@ void main(){
          break;
      case 5:
          printf("Enter a number: ");
-         inWord();
+         scanf("%d",&a);
+         inWord(a);
          break;
         
      case 6:
@@ -161,5 +215,6 @@ void main(){
          printf("ERROR! ENTER A VALID INPUT!");
          break;
      }
+    printf("\n\n");
    }
 }
